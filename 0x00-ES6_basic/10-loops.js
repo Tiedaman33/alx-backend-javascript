137a3d4d1f6d2a1ae045b58d0ab840449f28b2d1
Tiedaman33 <orinabravin6@gmail.com>
2024-08-05 21:40:22 +0000

    ES6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,10 @@
+/**
+ * Appends a string to each value in an array.
+ *
+ * @param {Array} array - The array to modify.
+ * @param {string} appendString - The string to append.
+ * @returns {Array} The modified array.
+ */
 export default function appendToEachArrayValue(array, appendString) {
   for (const [idx, value] of array.entries()) {
     array[idx] = appendString + value;
