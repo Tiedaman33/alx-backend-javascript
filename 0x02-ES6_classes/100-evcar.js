34a50e85277b419bf5d83a371b9120442766e087
Tiedaman33 <orinabravin6@gmail.com>
2024-08-08 22:17:31 +0000

    es60x02

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,18 +1,38 @@
 import Car from './10-car';
 
+/**
+ * Class representing an electric vehicle.
+ */
 export default class EVCar extends Car {
+  /**
+   * Create an EVCar.
+   * @param {string} brand - The brand of the car.
+   * @param {string} motor - The motor of the car.
+   * @param {string} color - The color of the car.
+   * @param {string} range - The range of the EV car.
+   */
   constructor(brand, motor, color, range) {
-    super(brand, motor, color);// Initialize Car attributes
+    super(brand, motor, color);
     this._range = range;
   }
 
-  // Override cloneCar to return an instance of Car
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Range must be a string');
+    }
+    this._range = value;
+  }
+
+  /**
+   * Clone the current EVCar instance and return a Car instance.
+   * @returns {Car} - A new Car instance.
+   */
   cloneCar() {
-    const clonedCar = super.cloneCar();
-    // Ensure clonedCar is an instance of Car
-    clonedCar._brand = this._brand;
-    clonedCar._motor = this._motor;
-    clonedCar._color = this._color;
+    const clonedCar = new Car(this._brand, this._motor, this._color);
     return clonedCar;
   }
 }
